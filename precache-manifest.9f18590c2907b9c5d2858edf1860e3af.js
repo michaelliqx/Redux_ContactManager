3424733484baa7dd2bc1b39c4d8f82dd37626408
Qingxing Li <lqx1996@bu.edu>
2019-02-11 20:35:48 -0500

    Updates

---
+++ precache-manifest.9f18590c2907b9c5d2858edf1860e3af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d0593af63dbfb29a8b2",
+    "url": "/Redux_ContactManager/static/js/runtime~main.6d0593af.js"
+  },
+  {
+    "revision": "34c30451269c2170fbfb",
+    "url": "/Redux_ContactManager/static/js/main.34c30451.chunk.js"
+  },
+  {
+    "revision": "16587fbc9adad8d35fe3",
+    "url": "/Redux_ContactManager/static/js/1.16587fbc.chunk.js"
+  },
+  {
+    "revision": "34c30451269c2170fbfb",
+    "url": "/Redux_ContactManager/static/css/main.3e3332d1.chunk.css"
+  },
+  {
+    "revision": "16587fbc9adad8d35fe3",
+    "url": "/Redux_ContactManager/static/css/1.08c8a3ea.chunk.css"
+  },
+  {
+    "revision": "d697bf60840bc15e61116c7e16cc3882",
+    "url": "/Redux_ContactManager/index.html"
+  }
+];
